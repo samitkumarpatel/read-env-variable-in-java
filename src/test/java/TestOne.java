b1084afaa446339e1c7f0d35e2faaa639c1211a8
samitkumarpatel <samitkumarpatel@gmail.com>
2020-12-23 16:24:25 +0100

    first commit

---
+++ src/test/java/TestOne.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestOne {
+
+	@Test
+	public void test() {
+		assertEquals("SAMIT", System.getProperty("NAME"));
+		
+	}
+
+}
